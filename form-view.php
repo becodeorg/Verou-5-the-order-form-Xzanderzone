0a871d14b8320793ad0368b4ed4f1ddaf0449124
PIeter <pieter_ophalvens@hotmail.com>
2024-01-05 10:15:37 +0100

    input validation and cookies to fill in data

--- form-view.php
+++ form-view.php
@@ -2,85 +2,92 @@
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
-          rel="stylesheet"/>
+        rel="stylesheet" />
     <title>Your fancy store</title>
 </head>
+
 <body>
-<div class="container">
-    <h1>Place your order</h1>
-    <?php // Navigation for when you need it ?>
-    <?php /*
-    <nav>
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="?food=1">Order food</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?food=0">Order drinks</a>
-            </li>
-        </ul>
-    </nav>
-    */ ?>
-    <form method="POST">
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" class="form-control"/>
-            </div>
-            <div></div>
-        </div>
-
-        <fieldset>
-            <legend>Address</legend>
-
+    <div class="container">
+        <h1>Place your order</h1>
+        <?php // Navigation for when you need it ?>
+        <?php /*
+<nav>
+<ul class="nav">
+<li class="nav-item">
+<a class="nav-link active" href="?food=1">Order food</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="?food=0">Order drinks</a>
+</li>
+</ul>
+</nav>
+*/?>
+        <form method="POST">
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" class="form-control" <?php if (isset($_COOKIE['email']))
+                        echo ('value="' . $_COOKIE['email'] . '"') ?> />
+                    </div>
+                    <div></div>
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control">
-                </div>
-            </div>
-        </fieldset>
 
-        <fieldset>
-            <legend>Products</legend>
-            <?php foreach ($products as $i => $product): ?>
-                <label>
-					<?php // <?= is equal to <?php echo ?>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
-                    &euro; <?= number_format($product['price'], 2) ?></label><br />
-            <?php endforeach; ?>
-        </fieldset>
+                <fieldset>
+                    <legend>Address</legend>
 
-        <button type="submit" class="btn btn-primary">Order!</button>
-    </form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="street">Street:</label>
+                            <input type="text" name="street" id="street" class="form-control" <?php if (isset($_COOKIE['street']))
+                        echo ('value="' . $_COOKIE['street'] . '"') ?>>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="streetnumber">Street number:</label>
+                            <input type="number" id="streetnumber" name="streetnumber" class="form-control" <?php if (isset($_COOKIE['streetnumber']))
+                        echo ('value="' . $_COOKIE['streetnumber'] . '"') ?>>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="city">City:</label>
+                            <input type="text" id="city" name="city" class="form-control" <?php if (isset($_COOKIE['city']))
+                        echo ('value="' . $_COOKIE['city'] . '"') ?>>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="zipcode">Zipcode</label>
+                            <input type="text" id="zipcode" name="zipcode" class="form-control" <?php if (isset($_COOKIE['zipcode']))
+                        echo ('value="' . $_COOKIE['zipcode'] . '"') ?>>
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Products</legend>
+                <?php foreach ($products as $i => $product): ?>
+                    <label>
+                        <input type="checkbox" value="1" name="products[<?php echo $i ?>]" /> <?php echo $product['name'] ?>
+                        -
+                        &euro; <?= number_format($product['price'], 2) ?></label><br />
+                <?php endforeach; ?>
+            </fieldset>
 
-    <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
-</div>
+            <button type="submit" class="btn btn-primary">Order!</button>
+        </form>
 
-<style>
-    footer {
-        text-align: center;
-    }
-</style>
+        <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
+    </div>
+
+    <style>
+        footer {
+            text-align: center;
+        }
+    </style>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -10,8 +10,9 @@ declare(strict_types=1);
 session_start();
 
 // Use this function when you need to need an overview of these variables
-function whatIsHappening() {
-    echo '<h2>$_GET</h2>';
+function whatIsHappening()
+{
+    echo '<pre><h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
     var_dump($_POST);
@@ -19,37 +20,64 @@ function whatIsHappening() {
     var_dump($_COOKIE);
     echo '<h2>$_SESSION</h2>';
     var_dump($_SESSION);
+    echo '</pre>';
 }
 
-// TODO: provide some products (you may overwrite the example)
+//  provide some products (you may overwrite the example)
 $products = [
-    ['name' => 'Your favourite drink', 'price' => 2.5],
+    ['name' => 'Your least favourite drink', 'price' => 2.5],
+    ['name' => 'Your favourite drink', 'price' => 25],
 ];
+
 
 $totalValue = 0;
 
 function validate()
 {
-    // TODO: This function will send a list of invalid fields back
-    return [];
+    $invalidFields = [];
+    // This function will send a list of invalid fields back
+    foreach ($_POST as $key => $value) {
+        setcookie($key, $value, time() + 3600, '/');
+        if ($value == '') {
+            $invalidFields[] = 'required field: ' . $key . '<br>';
+            continue;
+        } else {
+            if ($key == 'zipcode')
+                preg_match("/^[1-9]{1}[0-9]{3}$/i", $value) ?: $invalidFields[] = 'invalid zipcode';
+            else if ($key == 'email')
+                filter_var($value, FILTER_VALIDATE_EMAIL) ?: $invalidFields[] = 'The email address is incorrect';
+        }
+    }
+    return $invalidFields;
 }
-
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 function handleForm()
 {
-    // TODO: form related tasks (step 1)
+    // form related tasks (step 1)
 
+    whatIsHappening();
     // Validation (step 2)
     $invalidFields = validate();
     if (!empty($invalidFields)) {
-        // TODO: handle errors
+        // handle errors
+        foreach ($invalidFields as $problem) {
+            echo '<p style="color:red"> ' . $problem . '</p>';
+        }
     } else {
-        // TODO: handle successful submission
+        // handle successful submission
+        echo '<h2 style="background-color:green">Order submitted! Adress: ' . $_POST["street"] . $_POST["streetnumber"] . ', '
+            . $_POST['zipcode'] . ' ' . $_POST["city"] . ', confirmation email will be sent to: ' . $_POST["email"] . '</h2>';
     }
 }
 
-// TODO: replace this if by an actual check for the form to be submitted
-$formSubmitted = false;
-if ($formSubmitted) {
+// if (!empty($_POST)) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     handleForm();
 }
 
